00f6634d4e6141026bfbf8bfc44e504741073511
sangeethadev-12 <234g1a33f0@srit.ac.in>
2025-09-15 09:46:15 +0530

    Add files via upload

---
+++ java 1.js
@@ -0,0 +1,28 @@
+//print statement
+console.log("SRIT")
+
+//Data types in js
+
+num=12;
+str="hi";
+bo1=true;
+BigInt=752085208528520520;
+//undefined
+unde;
+null=null;
+
+//var,let,const
+
+var name1="srit";
+console.log(name1)
+var name1="hi";
+console.log(name1)
+let course="csm";
+let age=20;
+console.log(age)
+age=21;
+console.log(age)
+const givenname="srinivasa rit";
+console.log(givename)
+givenname="rit";
+console.log(givenname)
