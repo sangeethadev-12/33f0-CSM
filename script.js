ff51f0a2fd0f5ab856986f0836a0ebd060000024
sangeethadev-12 <234g1a33f0@srit.ac.in>
2025-09-15 09:51:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,92 @@
-//print statement
-console.log("SRIT")
+console.log("=== Hour 1: JSON BASIC ===");
 
-//Data types in js
+let student = {
+    name: "sangeetha",
+    age: 20,    
+    marks: [85,90,55]
+};
 
-num=12;
-str="hi";
-bo1=true;
-BigInt=752085208528520520;
-//undefined
+//convert object -> JSON string
+let jsonString = JSON.stringify(student);
+console.log("JSON String:", jsonString);
+console.log("Type of jsonString:", typeof jsonString);
 
-//var,let,const
+//convert JSON string -> object
+let parsedObject = JSON.parse(jsonString);
+console.log("parsed Object:", parsedObject);
 
-var name1="srit";
-console.log(name1)
-var name1="hi";
-console.log(name1)
-let course="csm";
-let age=20;
-console.log(age)
-age=21;
-console.log(age)
-const givenname="srinivasa srit";
-console.log(givenname)
+//variable of Books title ,price and author name
+let books = [
+    {title: "Atomic Habit", price: 200, authorName: "sathi"},
+    {title: "AI&ML", price: 150, authorName: "sangu"},
+    {title: "stanger Things", price: 300, authorName: "srav"}
+];  
+let booksJsonString = JSON.stringify(books);
+console.log("Books JSON String:", booksJsonString);
+let parsedBooks = JSON.parse(booksJsonString);
+console.log("Parsed Books Object:", parsedBooks);
+
+// Fetch sample json from API
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => response.json())
+  .then(data => console.log("Fetched Data:", data))
+  .catch(error => console.error('Error fetching data:', error));
+
+  //Fetch sample json and remove 1 post
+fetch('https://jsonplaceholder.typicode.com/posts/')
+  .then(response => response.json())
+  .then(data =>  console.log("Fetched All Posts:", data))
+  .catch(error => console.error('Error fetching data:', error));
+
+  // Hour 3: Dispaly json data on webpage
+
+  //Fetch all users and show in console + page
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(res => res.json())
+  .then(users => {  
+    let output = "Users List:\n";
+    users.forEach(user => {
+      output += `${user.name} - (${user.email})\n`;
+    });
+    console.log(output);
+  })
+  .catch(err => console.error(err));
+ 
+
+  const cityCoords = {
+    "banglore":{lat: 12.97, lon: 77.59},
+    "mumbai":{lat: 19.07, lon: 72.87},
+    "delhi":{lat: 28.61, lon: 77.20},
+    "chennai":{lat: 13.08, lon: 80.27},
+    "kolkata":{lat: 22.57, lon: 88.36}
+ };
+
+document.getElementById("fetchWeatherBtn").addEventListener("click",()=>{
+    let city = document.getElementById("cityInput").value.toLowerCase();
+
+    if(cityCoords[city]){
+       document.getElementById("weather").innerHTML = "⚠️City not in list";
+       return;
+    }
+ 
+    let coords=cityCoords[city];
+    let url=`https://api.open-meteo.com/v1/forecast?latitude=${coords.lat}&longitude=${coords.lon}&current_weather=true`;
+       
+    fetch(url)
+       .then(res => res.json())
+       .then(data => {
+          if(data.current_weather){
+             document.getElementById("weather").innerHTML =
+                `<h3>Weather in ${city}</h3>
+                <p> 🌡️Temp: ${data.current_weather.temperature}°C</p>
+                <p> 🌬️Wind: ${data.current_weather.windspeed} km/h</p>
+                <p> 🕒Time: ${data.current_weather.time}</p>`;
+          }
+          else{
+             document.getElementById("weather").innerHTML = "⚠️Weather data not available";
+          }
+       })
+       .catch(err => {
+          document.getElementById("weather").innerHTML = "⚠️Error fetching weather data";
+       });
+ });
